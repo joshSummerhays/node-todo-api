90a5242759e2c4878b8c401949c57d21e6aad57f
Josh Summerhays <russiansummer@gmail.com>
2016-12-15 15:24:41 -0700

    add delete/:id test

--- server/server.js
+++ server/server.js
@@ -67,7 +67,7 @@ app.delete('/todos/:id', (req, res) => {
         if(!todo){
             return res.status(404).send('todo not found');
         }
-        res.status(200).send(todo);
+        res.status(200).send({todo});
     }).catch((e) => {
         res.status(400).send('ERROR ERROR ERROR');
     });
