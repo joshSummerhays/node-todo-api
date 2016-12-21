b0e879a5b094e058f2ed10772d21b1c2c0288ce0
Josh Summerhays <russiansummer@gmail.com>
2016-12-20 17:38:14 -0700

    BUILD: hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-    id: 10
-};
+let password = '123abc!';
 
-let token = jwt.sign(data, '123abc');
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+let hashedPassword = '$2a$10$JdJOKolATZNs7FwnO1r5E.YMVUSeDQfvPaBjKL.NcIfP5eGz.IZbi';
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+    console.log(result);
+});
+
+// let data = {
+//     id: 10
+// };
+
+// let token = jwt.sign(data, '123abc');
 // console.log(token);
 
-let decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+// let decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 
 
 // let message = 'I am user 88';
